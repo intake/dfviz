e41efb322f1854cc8a71d032cd9669fe03b15895
Martin Durant <martin.durant@utoronto.ca>
2019-07-10 15:40:41 -0400

    update setup

--- setup.py
+++ setup.py
@@ -8,7 +8,8 @@ requires = open('requirements.txt').read().strip().split('\n')
 
 setup(
     name='dfviz',
-    version='0',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     description='dataframe interactive visualisation interface',
     classifiers=[
         'Development Status :: 4 - Beta',
@@ -19,15 +20,16 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
-    url='',
+    url='https://github.com/intake/dfviz',
     maintainer='Martin Durant',
     maintainer_email='mdurant@anaconda.com',
     license='BSD',
-    py_modules=['dfviz'],
     packages=find_packages(),
     package_data={'': ['*.csv', '*.yml', '*.html']},
     include_package_data=True,
     install_requires=requires,
     long_description=open('README.md').read(),
+    python_requires='>= 3.5',
+    keywords=['dataframe', 'plotting', 'visualization', 'interactive'],
     zip_safe=False,
 )
